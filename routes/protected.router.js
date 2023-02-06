a2067dd95d77518f8d750365f11149882f285341
vladimirsuhanov <vladimir.sukhanov99@gmail.com>
2023-02-07 00:48:23 +0300

    added userList endpoints

--- routes/protected.router.js
+++ routes/protected.router.js
@@ -1,4 +1,5 @@
 const {Router} = require("express");
+const User = require("../models/User");
 
 
 const router = Router()
@@ -9,6 +10,18 @@ router.get('/userInfo', async ({user}, res) =>{
         email,
         name,
         balance
+    })
+})
+
+router.get('/userList', async ({user}, res) =>{
+    const {id} = user;
+    const users = await User.find({_id: {$ne: id}});
+    res.status(200).json({
+        users: users?.map(x => ({
+            id: x.id,
+            email: x.email,
+            name: x.name
+        })) ?? []
     })
 })
 
